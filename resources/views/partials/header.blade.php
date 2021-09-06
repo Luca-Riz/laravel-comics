0cd869b36c5abedfce1e294c50ec0c9c38bf4014
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-06 14:54:16 +0200

    add part of the structure

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+  <div class="logo">
+    <img src="{{asset('img/logo.png')}}" alt="logo-dc">
+  </div>
+  <div class="main-menu">
+    <nav>
+      <ul>
+        <li><a href="{{route('homepage')}}">Home</a></li>
+
+      </ul>
+    </nav>
+  </div>
+</header>
