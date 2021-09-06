6cc14fe4ee7a7fc945d80b09249ae31aecb617b0
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-06 17:52:14 +0200

    add jumbotron

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,23 +1,34 @@
 <header>
-  <div class="logo">
-    <img src="{{asset('img/dc-logo.png')}}" alt="logo-dc">
+  <div class="menu">
+    <div class="logo">
+      <img src="{{asset('img/dc-logo.png')}}" alt="logo-dc">
+    </div>
+  
+    <div class="main-menu">
+      <nav>
+        <ul>
+          <li><a href="">Characters</a></li>
+          <li><a href="">Comics</a></li>
+          {{-- <li><a href="{{route('comics')}}">Comics</a></li> --}}
+          <li><a href="">Movies</a></li>
+          <li><a href="">Tv</a></li>
+          <li><a href="">Games</a></li>
+          <li><a href="">Collectibles</a></li>
+          <li><a href="">Videos</a></li>
+          <li><a href="">Fans</a></li>
+          <li><a href="">News</a></li>
+          <li><a href="">Shop</a></li>
+        </ul>
+      </nav>
+    </div>
+  
+    <div class="search">
+      <input type="text" placeholder="Seach">
+      <span>lente</span>
+    </div>
   </div>
-  <div class="main-menu">
-    <nav>
-      <ul>
-        <li><a href="">Characters</a></li>
-        <li><a href="">Comics</a></li>
-        {{-- <li><a href="{{route('comics')}}">Comics</a></li> --}}
-        <li><a href="">Movies</a></li>
-        <li><a href="">Tv</a></li>
-        <li><a href="">Games</a></li>
-        <li><a href="">Collectibles</a></li>
-        <li><a href="">Videos</a></li>
-        <li><a href="">Fans</a></li>
-        <li><a href="">News</a></li>
-        <li><a href="">Shop</a></li>
 
-      </ul>
-    </nav>
+  <div class="jumbotron">
+    <img src="{{asset('img/jumbotron.jpg')}}" alt="">
   </div>
 </header>
